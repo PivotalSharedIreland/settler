cdac44011ddd822176244cdd27bd6152522b12c6
Bengt Hammarlund and Luis Urraca <labs-pair+bhammarlund+lurraca@pivotal.io>
2016-01-22 09:40:50 +0000

    Added configuration of PropertyProvider

--- backend/src/main/java/io/pivotal/SettlerApplication.java
+++ backend/src/main/java/io/pivotal/SettlerApplication.java
@@ -2,11 +2,20 @@ package io.pivotal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SettlerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SettlerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SettlerApplication.class, args);
+    }
+
+    @Bean
+    public PropertyProvider propertyProvider() {
+        return new InMemoryPropertyProvider();
+    }
 }
+
+
+

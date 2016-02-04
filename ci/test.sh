e455205ac56ccf16f4f487f0814b2ac8cf08c01c
Brian Butz and Luis Urraca <labs-pair+bbutz+lurraca@pivotal.io>
2016-02-04 15:50:18 +0000

    Build tasks for CI

--- ci/test.sh
+++ ci/test.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
-
 set -e
-
-timestamp=`date +%s`
 
 export TERM=dumb
 export GRADLE_OPTS="-Dorg.gradle.native=false"
 
 cd settler-code
 ./gradlew -v
-./gradlew assemble -PVersion=$timestamp
+./gradlew test
